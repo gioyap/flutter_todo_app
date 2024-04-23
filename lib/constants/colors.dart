ed9663029d0c144f6b5e368899ef7580cb2ce069
Gio Yap <gioedrian.yap.l@gmail.com>
2024-04-23 13:14:35 +0800

    able to do crud and filtered data

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color.fromARGB(255, 255, 0, 0);
+const Color tdGreen = Color.fromARGB(255, 77, 235, 111);
+
+const Color tdBlack = Color(0xFF3A3A3A);
+const Color tdGrey = Color(0xFF717171);
+
+const Color tdBGColor = Color.fromARGB(255, 180, 191, 255);
